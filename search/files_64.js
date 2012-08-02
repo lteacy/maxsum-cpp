2ac9c565a839c44cf6984f3dec650ec97e5d3c83
Luke Teacy <wtlt@ecs.soton.ac.uk>
2012-08-02 14:13:23 +0100

    Updated documentation

--- search/files_64.js
+++ search/files_64.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['discretefunction_2ecpp',['DiscreteFunction.cpp',['../_discrete_function_8cpp.html',1,'']]],
+  ['discretefunction_2eh',['DiscreteFunction.h',['../_discrete_function_8h.html',1,'']]],
   ['domainiterator_2ecpp',['DomainIterator.cpp',['../_domain_iterator_8cpp.html',1,'']]],
   ['domainiterator_2eh',['DomainIterator.h',['../_domain_iterator_8h.html',1,'']]]
 ];
