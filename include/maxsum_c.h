6991d31a69bdef04a36a457d676cf32a42f529fe
Luke Teacy <wtlteacy@soton.ac.uk>
2014-04-24 23:22:42 +0100

    implemented error handling and sub2ind/ind2sub

--- include/maxsum_c.h
+++ include/maxsum_c.h
@@ -39,13 +39,38 @@ int getNumOfRegisteredVariables_ms();
  * Puts the specified variable in a global register, and stores its domain
  * size. Variables can be registered multiple times, but their domain size
  * must never change.
- * @throws InconsistentDomainException if this variable is already
- * registered, but with a different domain size.
+ * @returns -1 if this variable is already registered, but with a different
+ * domain size.
  * @param var the unique id of this variable
  * @param siz the domain size of this variable
  * @see maxsum::registerVariables
  */
-void registerVariable_ms(VarID var, ValIndex siz);
+int registerVariable_ms(VarID var, ValIndex siz);
+
+/**
+ * C implementation of matlab ind2sub function.
+ * @param idx the scalar linear index
+ * @param nDims length of dimension size vector
+ * @param pSiz input vector containing dimension sizes in order
+ * @param pSub output vector in which to store sub indices
+ * @return -1 on error, 0 otherwise.
+ */
+int ind2sub_ms
+(
+ const int idx,
+ const int nDims,
+ const int* const pSiz,
+ int* const pSub
+);
+
+/**
+ * C implementation of the matlab sub2ind function.
+ * @param nDims length of dimension size vector
+ * @param pSiz input vector containing dimension sizes in order
+ * @param pSub input vector in which to store sub indices
+ * @return -1 on error, or scalar linear index otherwise.
+ */
+int sub2ind_ms(const int nDims, const int* const pSiz, int* const pSub);
 
 #ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
 }
--- src/maxsum_c.cpp
+++ src/maxsum_c.cpp
@@ -4,6 +4,9 @@
  * 
  */
 
+#include <iostream>
+#include <algorithm>
+#include <vector>
 #include "maxsum_c.h"
 #include "maxsum/common.h"
 #include "maxsum/register.h"
@@ -25,12 +28,19 @@ int isRegistered_ms(maxsum::VarID var)
 /**
  * Returns the registered domain size for a specified variable.
  * @param var id of the variable to search for.
- * @returns domain size of var
- * @throws UnknownVariableException if the variable is not registered.
+ * @returns domain size of var, or -1 if variable is unknown.
  */
 maxsum::ValIndex getDomainSize_ms(maxsum::VarID var)
 {
-   return maxsum::getDomainSize(var);
+   try
+   {
+      return maxsum::getDomainSize(var);
+   }
+   catch(maxsum::UnknownVariableException& e)
+   {
+      std::cerr << e.what() << std::endl;
+      return -1;
+   }
 }
 
 /**
@@ -47,14 +57,95 @@ int getNumOfRegisteredVariables_ms()
  * Puts the specified variable in a global register, and stores its domain
  * size. Variables can be registered multiple times, but their domain size
  * must never change.
- * @throws InconsistentDomainException if this variable is already
- * registered, but with a different domain size.
+ * @returns -1 if this variable is already registered, but with a different
+ * domain size.
  * @param var the unique id of this variable
  * @param siz the domain size of this variable
  * @see maxsum::registerVariables
  */
-void registerVariable_ms(maxsum::VarID var, maxsum::ValIndex siz)
+int registerVariable_ms(maxsum::VarID var, maxsum::ValIndex siz)
 {
-   return maxsum::registerVariable(var,siz);
+   try
+   {
+      maxsum::registerVariable(var,siz);
+      return 0;
+   }
+   catch(maxsum::InconsistentDomainException& e)
+   {
+      std::cerr << e.what() << std::endl;
+      return -1;
+   }
+}
+
+/**
+ * C implementation of matlab ind2sub function.
+ * @param idx the scalar linear index
+ * @param nDims length of dimension size vector
+ * @param pSiz input vector containing dimension sizes in order
+ * @param pSub output vector in which to store sub indices
+ * @return -1 on error, 0 otherwise.
+ */
+int ind2sub_ms
+(
+ const int idx,
+ const int nDims,
+ const int* const pSiz,
+ int* const pSub
+)
+{
+   if(0>=nDims)
+   {
+      return -1;
+   }
+
+   try
+   {
+      // create vectors to hold input and output
+      std::vector<int> sizVec(nDims);
+      std::vector<int> subVec(nDims);
+
+      // copy input into vector
+      std::copy(pSiz,pSiz+nDims,sizVec.begin());
+
+      // call C++ version to do the hard work
+      maxsum::ind2sub(sizVec, idx, subVec);
+
+      // copy output to parent function
+      std::copy(subVec.begin(),subVec.end(),pSub);
+
+      // indicate that all went well
+      return 0;
+
+   }
+   catch(maxsum::OutOfRangeException& e)
+   {
+      std::cerr << e.what() << std::endl;
+      return -1;
+   }
+}
+
+/**
+ * C implementation of the matlab sub2ind function.
+ * @param nDims length of dimension size vector
+ * @param pSiz input vector containing dimension sizes in order
+ * @param pSub input vector in which to store sub indices
+ * @return -1 on error, or scalar linear index otherwise.
+ */
+int sub2ind_ms(const int nDims, const int* const pSiz, int* const pSub)
+{
+   if(0>=nDims)
+   {
+      return -1;
+   }
+
+   try
+   {
+      return maxsum::sub2ind(pSiz, pSiz+nDims, pSub, pSub+nDims);
+   }
+   catch(maxsum::OutOfRangeException& e)
+   {
+      std::cerr << e.what() << std::endl;
+      return -1;
+   }
 }
 
